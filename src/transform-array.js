b9c52aa1e582b4e71602d0853d465ae121c147a0
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-17 03:37:39 +0600

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,26 +5,34 @@ module.exports = function transform(arr) {
   
   for(i=0; i<arr2.length; i++){
     if(arr2[i]=='--discard-prev' && i!=0){
-      arr2.splice(i, 1)
-      arr2.splice(i-1, 1)
+      delete arr2[i]
+      delete arr2[i-1]
+      //arr2.splice(i, 1)
+      //arr2.splice(i-1, 1)
     }else if(arr2[i]=='--discard-prev' && i==0){
-      arr2.splice(i, 1)}
+      delete arr2[i]
+      //arr2.splice(i, 1)}
     else if(arr2[i]=='--discard-next' && i!=(arr2.length-1)){
-      arr2.splice(i, 1)
-      arr2.splice(i+1, 1)
+      delete arr2[i]
+      delete arr2[i+1]
+      //arr2.splice(i, 1)
+      //arr2.splice(i+1, 1)
     }else if(arr2[i]=='--discard-next' && i==(arr2.length-1)){
-      arr2.splice(i, 1)
+      delete arr2[i]
+      //arr2.splice(i, 1)
     }
     else if(arr2[i]=='--double-next' && i!=(arr2.length-1)){
-     
+      
       arr2[i]=arr2[i+1];
     }else if(arr2[i]=='--double-next' && i==(arr2.length-1)){
-      arr2.splice(i, 1)}
+      delete arr2[i]
+      //arr2.splice(i, 1)}
    else if(arr2[i]=='--double-prev' && i!=0){
      
       arr2[i]=arr2[i-1];
     }else if(arr2[i]=='--double-prev' && i==0){
-      arr2.splice(i, 1)
+      delete arr2[i]
+      //arr2.splice(i, 1)
   }}
   return arr2;
 };
