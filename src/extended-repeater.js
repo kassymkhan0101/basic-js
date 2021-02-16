ccf616527a30158746c370a13b4bf836f052641d
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-17 00:39:56 +0600

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
   throw new CustomError('Not implemented');
-  str = str+(options.addition)+(options.additionSeparator+options.addition)*(options.additionRepeatTimes-1)+(options.separator+str+(options.addition)+(options.additionSeparator+options.addition)*(options.additionRepeatTimes-1))*(options.repeatTimes-1)
+  str = str+(options.addition)+(options.additionSeparator+options.addition).repeat(options.additionRepeatTimes-1)+(options.separator+str+(options.addition)+(options.additionSeparator+options.addition).repeat(options.additionRepeatTimes-1)).repeat(options.repeatTimes-1)
   return str;
   
 };
