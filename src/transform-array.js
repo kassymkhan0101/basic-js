34bf67597b2d06312f969281a1725438c9447ac8
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-17 03:08:27 +0600

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,18 +4,18 @@ module.exports = function transform(arr) {
   let arr2 = arr.slice();
   
   for(i=0; i<arr2.length; i++){
-    if(arr2[i]=='--discard-next'){
+    if(arr2[i]=='--discard-prev' && i!=0){
       arr2.splice(i, 1)
       arr2.splice(i-1, 1)
     }
-    if(arr2[i]=='--discard-next'){
+    if(arr2[i]=='--discard-next' && i!=(arr2.length-1)){
       arr2.splice(i, 1)
       arr2.splice(i+1, 1)
     }
-    if(arr2[i]=='--double-next'){
+    if(arr2[i]=='--double-next' && i!=(arr2.length-1)){
       arr2[i]=arr2[i+1];
     }
-    if(arr2[i]=='--double-prev'){
+    if(arr2[i]=='--double-prev' && i!=0){
       arr2[i]=arr2[i-1];
     }
   }
