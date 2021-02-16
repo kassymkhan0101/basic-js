4e0be66723a7538192f5d2525b850f3c7af54375
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-16 18:51:13 +0600

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,15 +1,15 @@
 const CustomError = require("../extensions/custom-error");
-let c = 0;
 module.exports = function countCats(backyard) {
-  throw new CustomError('Not implemented');
+  let c =0;
   
   for(i=0; i<backyard.length; i++){
     for(j=0; j<i.length; j++){
       
       if(backyard[i][j]==="^^" || backyard[i][j]==='^^'){
-        c += 1;
-        return c;
+        
+        c++;
       }
     }
   }
+  return c;
 };
