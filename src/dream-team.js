7b9cf7bc73a77bb8ccc72dee65f334257e04ebd7
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-17 02:27:24 +0600

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -5,6 +5,8 @@ module.exports = function createDreamTeam(arr) {
   for(i=0; i<arr.length; i++){
     if(typeof arr[i] == 'string' && arr[i].length != 0){
       names.push(arr[i].trim().charAt(0));
+    }else{
+      return false;
     }
   }
   return names.sort().join("").toUpperCase();
