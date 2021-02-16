2906d7baa81d071cf18ab82b79a34c54f8ca8004
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-17 01:56:31 +0600

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -2,11 +2,11 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(k) {
   let c = k.getMonth();
-  if(c==0 || c==1 ||c==2){
+  if(c==11 || c==0 ||c==1){
     return 'winter';
-  }else if(c==3 || c==4 ||c==5){
+  }else if(c==2 || c==3 ||c==4){
     return 'spring';
-  }else if(c==6 || c==7 ||c==8){
+  }else if(c==5 || c==6 ||c==7){
     return 'summer';
   }else{
     return 'autumn'
