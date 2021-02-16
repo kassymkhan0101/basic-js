7ae1f9d39c3176337ff71dc08d67188d7f295799
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-16 18:22:10 +0600

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,11 @@
 const CustomError = require("../extensions/custom-error");
-
+let c = 0;
 module.exports = function countCats(k) {
-  let c  = 0
+  throw new CustomError('Not implemented');
+  
   for(i=0; i<k.length; i++){
     for(j=0; j<i.length; j++){
+      
       if(k[i][j]=="^^"){
         c++;
         return c;
