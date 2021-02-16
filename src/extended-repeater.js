513b9fc04962efb9d42d028276e01abce7bc9057
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-17 01:03:23 +0600

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,13 +1,13 @@
 
 
 module.exports = function repeater(str, options) {
-  if(options.separator==null || options.separator==undefined || options.separator==""){
+  if(options.separator==null || options.separator==undefined){
     options.separator='+';
   }
-  if(options.additionSeparator==null || options.additionSeparator==undefined || options.additionSeparator== ""){
+  if(options.additionSeparator==null || options.additionSeparator==undefined){
     options.additionSeparator='|';
   }
-  if(options.addition==null || options.addition==undefined || options.addition==""){
+  if(options.addition==null || options.addition==undefined){
     options.separator='';
   }
   str = String(str)+(options.addition)+(options.additionSeparator+options.addition).repeat((options.additionRepeatTimes)-1)+(options.separator+str+(options.addition)+(options.additionSeparator+options.addition).repeat((options.additionRepeatTimes)-1)).repeat((options.repeatTimes)-1)
