cab5fa4c579f170378658d129efd5fa7e93c81a0
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-17 03:16:22 +0600

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,17 +7,24 @@ module.exports = function transform(arr) {
     if(arr2[i]=='--discard-prev' && i!=0){
       arr2.splice(i, 1)
       arr2.splice(i-1, 1)
-    }
+    }else if(arr2[i]=='--discard-prev' && i==0){
+      arr2.splice(i, 1)
     if(arr2[i]=='--discard-next' && i!=(arr2.length-1)){
       arr2.splice(i, 1)
       arr2.splice(i+1, 1)
+    }else if(arr2[i]=='--discard-next' && i==(arr2.length-1)){
+      arr2.splice(i, 1)
     }
     if(arr2[i]=='--double-next' && i!=(arr2.length-1)){
+      arr2.splice(i, 1)
       arr2[i]=arr2[i+1];
-    }
+    }else if(arr2[i]=='--double-next' && i==(arr2.length-1)){
+      arr2.splice(i, 1)
     if(arr2[i]=='--double-prev' && i!=0){
+      arr2.splice(i, 1)
       arr2[i]=arr2[i-1];
-    }
+    }else if(arr2[i]=='--double-prev' && i==0){
+      arr2.splice(i, 1)
   }
   return arr2;
 };
