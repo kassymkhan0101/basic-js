c17b51de99d00a9096059d0852e0d49082cad29f
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-16 18:57:37 +0600

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(backyard) {
-  
+  let c = 0;
   for(i=0; i<backyard.length; i++){
-    for(j=0; j<i.length; j++){
+    for(j=0; j<backyard[i]; j++){
       
       if(backyard[i][j]=="^^"){
         c += 1;
