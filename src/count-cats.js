bbb1e792d207a5161992edd827cf60a418f8bbb6
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-16 18:47:35 +0600

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -6,8 +6,8 @@ module.exports = function countCats(backyard) {
   for(i=0; i<backyard.length; i++){
     for(j=0; j<i.length; j++){
       
-      if(backyard[i][j]=="^^" || backyard[i][j]=="^ ^"){
-        c++;
+      if(backyard[i][j]==="^^" || backyard[i][j]==='^^'){
+        c += 1;
         return c;
       }
     }
